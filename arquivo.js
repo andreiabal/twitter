f39f59bcd23a9582fb43c423af562b2bf8dd8842
Andreia Bal <bal.andreia@gmail.com>
2018-07-09 22:05:37 -0300

    0.0.5

--- arquivo.js
+++ arquivo.js
@@ -1,17 +1,46 @@
-// 1. criar funcao no submit
+// onSubmit - passos:
+// 1. criar função no submit
 // 2. pegar o texto do input
 // 3. criar li
 // 4. colocar texto no li
 // 5. colocar o elemento li no ul
 
 function onSubmit() {
-	var userText = document.getElementById('text').value;
+  var userText = document.getElementById('text').value;
   var listItem = document.createElement('li');
   listItem.className = 'twitte';
   listItem.innerHTML = userText;
   document.getElementById('posts').appendChild(listItem);
-
+  var cleanText = document.getElementById('text');
+  cleanText.value = '';
+  char(); //atualizando a contagem dos caracteres
   return false;
+}
+
+//Contar caracteres e alterar as cores do contador
+function char() {
+  var max = new Number();
+  max = 140;
+  var initial = document.getElementById('text').value.length;
+  var counting = new Number();
+  counting = max - initial;
+  document.getElementById('count').innerHTML = counting;
+
+  if (counting < 20 && counting > 10) {
+    document.getElementById('count').style.color='orange';
+  }
+  if (counting < 10 && counting >= 0) {
+    document.getElementById('count').style.color='red';
+  }
+  if (counting >= 20) {
+    document.getElementById('count').style.color='#6AD1F2';
+  }
+  // Atualizar o status do botão
+  if(counting >= 0 && counting < 140) {
+  	document.getElementById('button').disabled='';
+  } else {
+    document.getElementById('button').disabled='disabled';
+  }
 }
 
 
